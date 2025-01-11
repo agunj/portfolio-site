3d50400a7b2000935e06c918fca68b38ece71ffc
AdvaitG <82006776+agunj@users.noreply.github.com>
2025-01-10 22:36:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.addEventListener('load', function() {
-    // Get the content element
-    const content = document.querySelector('.content');
-
-    // Wait for the circle animation to complete (3 seconds)
-    setTimeout(() => {
-        // Display the content after 3 seconds
-        content.style.display = 'block';
-    }, 3000); // Wait for the duration of the circle animation
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
 });
