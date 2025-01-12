56c36cd84ef5520cc37630544dae2013a505e5d2
AdvaitG <82006776+agunj@users.noreply.github.com>
2025-01-12 05:30:53 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const button = document.getElementById('showCircleBtn');
+const circle = document.getElementById('circle');
+const contact = document.getElementById('contactButton');
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+button.addEventListener('click', function() {
+  circle.style.visibility = 'visible'; // Make the circle visible
+  circle.style.opacity = 1; // Fade in the circle
+  circle.style.width = '2000px'; // Set the size of the circle
+  circle.style.height = '2000px'; // Set the size of the circle
+
+});
+contact.addEventListener('click', function() {
+  circle.style.visibility = 'visible'; // Make the circle visible
+  circle.style.opacity = 1; // Fade in the circle
+  circle.style.width = '2000px'; // Set the size of the circle
+  circle.style.height = '2000px'; // Set the size of the circle
+  
 });
