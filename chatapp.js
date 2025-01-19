6e69325aa35366fc5b6e821e7d753ff2ad509d91
AdvaitG <82006776+agunj@users.noreply.github.com>
2025-01-18 23:26:13 -0500

    Update chatapp.js

--- chatapp.js
+++ chatapp.js
@@ -1,4 +1,4 @@
-import { HfInference } from "../node_modules/@huggingface/inference";
+import { HfInference } from "./node_modules/@huggingface/inference";
 
 // Replace with your HuggingFace token
 const client = new HfInference("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");
